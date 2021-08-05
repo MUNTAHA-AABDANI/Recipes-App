02e00e5e0be1b4cb8c8f5efebc434ea7ff30b6ec
Muntaha-Aabdani <engineersidra19@gmail.com>
2021-08-05 06:13:48 -0700

    Recipes App

---
+++ app.js
@@ -0,0 +1,45 @@
+const API_Key = "20397067c4ea4412be53801c9a7d0200"; //Api key
+
+const query = "pasta"; //The requirement which recipies you want to show//
+const ShowPasta = document.querySelector('.ShowPasta'); //target the div whose class name 'ShowPasta'
+
+
+
+fetch(` https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_Key}&query=${query}`).then(function(response) {
+    return response.json();
+}).then(function(data) {
+    console.log(data);
+
+    data.results.forEach(results => {
+        ShowPasta.innerHTML += `
+        <div class="card-container   ">
+        <div class="flex-card">
+                    <div class="card mt-5 mb-3" style="max-width: 540px;">
+                        <div class="row no-gutters">
+                            <div class=" col-6">
+                                <div class="card-body">
+        
+                                    <h3 class="card-title">${results.title}</h3>
+        
+                                    <p class="desc card-text">Delicious Mouth watering pasta recipies make your own pasta at home with simple recipy</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <button type="button" class="btn btn-danger ">Get Recipes</button> <br> <br>
+                                    <a class="link text-danger" href="#">License: CC BY-SA 3.0</a>
+                                </div>
+        
+                            </div>
+                            <div class=" col-6 ">
+                                <span class="icon"><i class="fa fa-bookmark-o" aria-hidden="true"></i></span>
+                                <img src="${results.image}" class="card-img" alt="Pasta-img">
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+        
+        `
+
+    });
+
+
+})
+
